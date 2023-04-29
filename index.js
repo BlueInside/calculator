3efa6078fd76e150080c6afccf8e6f17504295a1
Karol <kpulawski13@gmail.com>
2023-04-29 20:41:27 +0100

    create operate func

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+function operate(num1, operator, num2) {
+  if (operator === "+") return add(num1, num2);
+  if (operator === "-") return subtract(num1, num2);
+  if (operator === "*") return multiply(num1, num2);
+  if (operator === "/") return divide(num1, num2);
+}
