00b942df05b79a7a85cdc1c635a53038f313ce60
Karol <kpulawski13@gmail.com>
2023-05-02 09:29:00 +0100

    create variables for nums and operator

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-function operate(num1, operator, num2) {
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(num1, num2, operator) {
   if (operator === "+") return add(num1, num2);
   if (operator === "-") return subtract(num1, num2);
   if (operator === "*") return multiply(num1, num2);
